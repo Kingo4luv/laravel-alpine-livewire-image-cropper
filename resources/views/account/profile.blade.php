a550aef60e3f282a8b23e480205a55840005af6e
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-08-04 20:04:08 +0100

    changed container size

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-3 col-sm-8">
+        <div class="col-lg-5 col-md-3 col-sm-8">
             <div class="card">
                 <div class="card-header">Profile</div>
                 <div class="card-body">
